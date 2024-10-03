c45b85032b641fbb9409fc82bb7f20ac8674bc98
unknown <betuloran2@gmail.com>
2024-10-03 13:44:15 +0300

    express app created

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,10 @@ const express=require('express')
 
 const app=express();
 
-app.listen=
+app.listen(4000, ()=>{
+    console.log('4000. port dinleniyor');
+})
+
+app.get('/',(req,res)=>{ //req, gelen istekleri yakalayan parametre. res, cevap verebilmek için kullanılır 
+res.json({mesaj:"merhaba express app"})
+})
