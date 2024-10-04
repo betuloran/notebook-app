25570bfca2d9b8545b7a2cd272c4b80ac1e5571c
unknown <betuloran2@gmail.com>
2024-10-04 11:38:53 +0300

    .env file edited

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express=require('express')
 require('dotenv').config()
+
 const app=express();
+
+app.use((req,res,next)=>{
+console.log(req.path,req.method);
+next();
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log(`${process.env.PORT}. port dinleniyor`);
