b44c8e07217f7bb6576b1da06c4f5ca1bb440450
unknown <betuloran2@gmail.com>
2024-10-06 22:33:55 +0300

    Api routers created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -8,4 +8,23 @@ router.get('/',(req,res)=>{ //'/' yoluna yapılan her GET isteği bu route taraf
 //kök yoluna (yani '/') yapılan bir istek geldiğinde, sunucu bir JSON yanıtı döner: { msg: 'bütün notlar' }.
 // Bu, istemciye bir JSON yanıtı olarak "bütün notlar" mesajını döner.
 
+//listeleme işlemi
+router.get('/:id',(req,res)=>{ 
+    res.json({msg:`${req.params.id} id'li notu getir`})
+})
+
+//ekleme işlemi
+router.post('/',(req,res)=>{ 
+    res.json({msg:'yeni not ekle'})
+})
+
+//silme işlemi
+router.delete('/:id',(req,res)=>{ 
+    res.json({msg:`${req.params.id} id'li notu sil`})
+})
+
+//güncelleme işlemi
+router.patch('/:id',(req,res)=>{ 
+    res.json({msg:`${req.params.id} id'li notu güncelle`})
+})
 module.exports=router;//Bu satır, oluşturulan router'ı dışa aktarır yani başka dosyalardan bu router'a erişilip kullanılabilir
