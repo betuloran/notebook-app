eeabd758d797bda13ed1271264240e3106c66e4b
unknown <betuloran2@gmail.com>
2024-10-07 15:40:55 +0300

    notes.js updated

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,30 +1,38 @@
 
-const express=require('express') //route işlemleri için express kullanıyoruz bu yüzden burada espress'i çağırdık 
-const router=express.Router() //express içindeki router'i oluşturduk
+const express = require('express') //route işlemleri için express kullanıyoruz bu yüzden burada espress'i çağırdık 
+const router = express.Router() //express içindeki router'i oluşturduk
+const notModel=require('../models/notModel')
 
-router.get('/',(req,res)=>{ //'/' yoluna yapılan her GET isteği bu route tarafından yakalanır.
-    res.json({msg:'bütün notlar'})
+router.get('/', (req, res) => { //'/' yoluna yapılan her GET isteği bu route tarafından yakalanır.
+    res.json({ msg: 'bütün notlar' })
 })
 //kök yoluna (yani '/') yapılan bir istek geldiğinde, sunucu bir JSON yanıtı döner: { msg: 'bütün notlar' }.
 // Bu, istemciye bir JSON yanıtı olarak "bütün notlar" mesajını döner.
 
 //listeleme işlemi
-router.get('/:id',(req,res)=>{ 
-    res.json({msg:`${req.params.id} id'li notu getir`})
+router.get('/:id', (req, res) => {
+    res.json({ msg: `${req.params.id} id'li notu getir` })
 })
 
 //ekleme işlemi
-router.post('/',(req,res)=>{ 
-    console.log(req);
+router.post('/', async (req, res) => {
+    const { baslik, aciklama } = req.body
+    try {
+const not=await notModel.create({baslik,aciklama})
+res.status(200).json(not)
+    }
+    catch (error) {
+        res.status(400).json({hata:error.message})
+    }
 })
 
 //silme işlemi
-router.delete('/:id',(req,res)=>{ 
-    res.json({msg:`${req.params.id} id'li notu sil`})
+router.delete('/:id', (req, res) => {
+    res.json({ msg: `${req.params.id} id'li notu sil` })
 })
 
 //güncelleme işlemi
-router.patch('/:id',(req,res)=>{ 
-    res.json({msg:`${req.params.id} id'li notu güncelle`})
+router.patch('/:id', (req, res) => {
+    res.json({ msg: `${req.params.id} id'li notu güncelle` })
 })
-module.exports=router;//Bu satır, oluşturulan router'ı dışa aktarır yani başka dosyalardan bu router'a erişilip kullanılabilir
+module.exports = router;//Bu satır, oluşturulan router'ı dışa aktarır yani başka dosyalardan bu router'a erişilip kullanılabilir
