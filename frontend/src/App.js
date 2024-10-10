9101ed2065eb992fa810d6233e8746cd96df2a3a
unknown <betuloran2@gmail.com>
2024-10-10 23:32:38 +0300

    project updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-     
-
+      <BrowserRouter>
+        <div className='pages'>
+          <Routes>
+<Route path='/' element={<Home/>} />  
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <h2>anasayfa</h2>
+    </div>
+  )
+}
