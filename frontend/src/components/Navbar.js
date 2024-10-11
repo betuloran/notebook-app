f65c868d3b71c2d99c691740b163f7e84b3add28
unknown <betuloran2@gmail.com>
2024-10-11 12:46:46 +0300

    css properties added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <header>
-      
+      <div className='container'>
+        <Link>
+          <h1 to='/'>
+            AOS NOT DEFTERİ
+          </h1>
+        </Link>
+      </div>
     </header>
   )
 }
