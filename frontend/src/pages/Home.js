1e1965766397dc5820742a569aae3c2a20f3bd7e
unknown <betuloran2@gmail.com>
2024-10-16 20:37:30 +0300

    home.js updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,29 +1,33 @@
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import React from 'react'
 
 export default function Home() {
 
-const [notlar,setNotlar]=useState(null) // state değişkenini ve onu güncellemek için setState fonksiyonunu tanımlıyoruz
-  
-useEffect(()=>{
+  const [notlar, setNotlar] = useState(null) // state değişkenini ve onu güncellemek için setState fonksiyonunu tanımlıyoruz
 
-const fetchNotlar=async ()=>{
-  const response=await fetch('http://localhost:4000/api/notlar') //get işlemi gerçekleştirilecek 
-  const json=await response.json()
+  useEffect(() => {
 
-  if(response.ok){
-    setNotlar.json
-  }
-}
-fetchNotlar()
-},[])
-return (
+    const fetchNotlar = async () => {
+      const response = await fetch('/api/notlar') //get işlemi gerçekleştirilecek 
+      
+      const json = await response.json()
+
+      console.log(json);
+      if (response.ok) {
+        setNotlar(json)
+      }
+    }
+    fetchNotlar()
+
+  }, [])
+
+  return (
     <div className="home">
-       <div className='notlar'>
-{notlar && notlar.map((not)=>{
-  
-})}
-       </div>
+      <div className='notlar'>
+        {notlar && notlar.map((not) => {
+          <p key={not._id}>{not.baslik}</p>
+        })}
+      </div>
     </div>
   )
 
