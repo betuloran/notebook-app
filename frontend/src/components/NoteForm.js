77047026ff82447d5812bfda408d90a1b2d349a8
unknown <betuloran2@gmail.com>
2024-10-17 12:27:48 +0300

    The process of adding notes

--- frontend/src/components/NoteForm.js
+++ frontend/src/components/NoteForm.js
@@ -4,13 +4,32 @@ import { useState } from 'react'
 export default function NoteForm() {
 const [baslik,setBaslik]=useState('')
 const [aciklama,setAciklama]=useState('')
+const [hata,setHata]=useState(null)
 
-const handleSubmit=(e)=>{
+const handleSubmit=async (e)=>{
     e.preventDefault();
     const not={baslik,aciklama}
-    console.log(not);
-}
+    // console.log(not);
 
+    const response=await fetch('/api/notlar',{
+      method:'POST',
+      body:JSON.stringify(not),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    const json=await response.json()
+
+    if(!response.ok){
+      setHata(json.hata)
+    }
+    if(response.ok){
+      setHata(null)
+      setBaslik('')
+      setAciklama('')
+      console.log('yeni bir not eklendi',json);
+    }
+}
 
   return (
     <form className='create' onSubmit={handleSubmit}>
@@ -25,6 +44,7 @@ const handleSubmit=(e)=>{
         <input type='text' onChange={(e)=>setAciklama(e.target.value)} value={aciklama}/>
       </div>
       <button type='submit'>EKLE</button>
+      {hata && <div className='error'>{hata}</div>}
     </form>
   )
 }
